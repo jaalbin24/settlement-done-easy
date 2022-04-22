36b8e72f50920b5471f122a1db6bd744845a91ab
Jeremy <thefirstjeremy@gmail.com>
2022-04-22 14:58:14 -0500

    Experimenting with test fixtures

---
+++ test/system/settlements_test.rb
@@ -0,0 +1,10 @@
+require "application_system_test_case"
+
+class SettlementsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit settlements_url
+  #
+  #   assert_selector "h1", text: "Settlements"
+  # end
+
+end
