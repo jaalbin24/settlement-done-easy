b5dfd322f66fa2e42816cb50eb0abd58d258ac14
Jeremy Albin <thefirstjeremy@gmail.com>
2022-08-31 16:00:24 -0500

    YES

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,4 +1,11 @@
 module ApplicationCable
-  class Channel < ActionCable::Channel::Base
-  end
+    class Channel < ActionCable::Channel::Base
+        identified_by :current_user
+
+        def connect
+            if user_signed_in?
+                self.current_user = current_user
+            end
+        end
+    end
 end
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: addresses
+#
+#  id          :bigint           not null, primary key
+#  city        :string
+#  country     :string
+#  line1       :string
+#  line2       :string
+#  postal_code :integer
+#  state       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Address < ApplicationRecord
 end
