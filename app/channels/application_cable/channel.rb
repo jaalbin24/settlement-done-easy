5b433f9a015b81a0ffba66c4889c3b27f55025a3
Jeremy Albin <thefirstjeremy@gmail.com>
2022-08-31 18:28:57 -0500

    YES

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,11 +1,12 @@
 module ApplicationCable
     class Channel < ActionCable::Channel::Base
-        identified_by :current_user
 
-        def connect
-            if user_signed_in?
-                self.current_user = current_user
-            end
+        def subscribed
+
+        end
+
+        def unsubscribed
+
         end
     end
 end
