b405e76c064629a6de47914bd188fdb3560dfad6
Jeremy Albin <hthefirstjeremy@gmail.com>
2022-09-30 08:26:46 -0500

    Logging off Ubuntu

--- spec/system/user_signs_in_spec.rb
+++ spec/system/user_signs_in_spec.rb
@@ -1,8 +1,9 @@
 RSpec.describe "User signs in" do
-    scenario "and they start at the home page" do
+    fixtures :users
+    scenario "from the home page" do
         visit root_path
         click_on "Sign in"
-        fill_in "Email", with: "insurance_company0@example.com"
+        fill_in "Email", with: "state_farm.test@example.com"
         fill_in "Password", with: "password123"
         click_button "Sign in"
         expect(page).to have_text "State Farm"
