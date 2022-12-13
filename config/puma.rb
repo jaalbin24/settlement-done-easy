68ce77fee0f40d85f9637121bf6c5b916eac18c8
Jeremy <thefirstjeremy@gmail.com>
2022-12-12 20:53:48 -0600

    Another minor change to trigger AWS services

--- config/puma.rb
+++ config/puma.rb
@@ -29,7 +29,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # the concurrency of the application would be max `threads` * `workers`.
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
-#
+
 workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
