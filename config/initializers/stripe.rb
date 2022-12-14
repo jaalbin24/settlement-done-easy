f693b895489192858d987b9dbd87cced46e218c9
Jeremy <thefirstjeremy@gmail.com>
2022-12-14 14:42:28 -0600

    Whoops

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,1 +1,5 @@
-Stripe.api_key = Rails.configuration.STRIPE_SECRET_KEY
+if Rails.env.production?
+    Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+else
+    Stripe.api_key = Rails.configuration.STRIPE_SECRET_KEY
+end
