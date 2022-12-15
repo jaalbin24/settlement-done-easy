5ed1adb5b80ae0de7ade72eebf82e93e6cf50f50
Jeremy <thefirstjeremy@gmail.com>
2022-12-14 23:22:40 -0600

    More stuff

--- config/initializers/appsettings.rb
+++
@@ -1,6 +0,0 @@
-# require "rails/all"
-
-# CONFIG = YAML.load_file("#{Rails.root.to_s}/config/appsettings.yml")[Rails.env]
-# CONFIG.map do |k,v|
-#   SettlementDoneEasy::Application.config.send("#{k}=", v)
-# end
