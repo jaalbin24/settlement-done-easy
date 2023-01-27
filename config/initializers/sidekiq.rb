8b24ac1c8549453cb93392bd1e03152a1c09fd87
Jeremy Albin <thefirstjeremy@gmail.com>
2023-01-27 12:19:29 -0600

    Now using Foreman to start the app in dev mode

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,8 @@
 if Rails.env.development?
     Sidekiq.configure_server do |config|
-        config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379') }
+        config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
     end
     Sidekiq.configure_client do |config|
-        config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379') }
+        config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
     end
 end
