2cfd31439e029018241309971de926d8f4b6008b
Jeremy Albin <thefirstjeremy@gmail.com>
2023-01-27 15:32:19 -0600

    Redis queue names are not in sync with aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   config.active_job.queue_adapter = :sidekiq
   Sidekiq.configure_server { |c| c.redis = { url: ENV['REDIS_URL'] } }
-  # config.active_job.queue_name_prefix = "sde_production"
+  # config.active_job.queue_name_prefix = "sde_production" NO
 
   config.action_mailer.perform_caching = false
 
